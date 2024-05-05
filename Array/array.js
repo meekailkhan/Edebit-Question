d187a2aeafe981b0dfc1e46e6a2abd9a9bacbb06
meekailkhan <meekailkhan123aar@gmail.com>
2024-05-05 23:48:18 +0530

    in this repo we solve daily random questions of javascript

--- Array/array.js
+++ Array/array.js
@@ -511,12 +511,38 @@
 
 // testJackpot(["SS", "SS", "SS", "Ss"]) ➞ false
 // ans)=>
-function testJackpot(arr){
-    return arr.every(item => item===arr[0])
+// function testJackpot(arr){
+//     return arr.every(item => item===arr[0])
+// }
+
+// console.log(testJackpot(["@", "@", "@", "@"]))// ➞ true
+// console.log(testJackpot(["abc", "abc", "abc", "abc"])) // ➞ true
+// console.log(testJackpot(["SS", "SS", "SS", "SS"])) // ➞ true
+// console.log(testJackpot(["&&", "&", "&&&", "&&&&"])) // ➞ false
+// console.log(testJackpot(["SS", "SS", "SS", "Ss"])) // ➞ false
+// -----------------------------------------------------------------------------------------------------------------
+
+// Question)=> Create a function that takes an array of numbers arr, a string str and return an array of numbers as per the following rules:
+
+// "Asc" returns a sorted array in ascending order.
+// "Des" returns a sorted array in descending order.
+// "None" returns an array without any modification.
+// Examples
+// ascDesNone([4, 3, 2, 1], "Asc" ) ➞ [1, 2, 3, 4]
+
+// ascDesNone([7, 8, 11, 66], "Des") ➞ [66, 11, 8, 7]
+
+// ascDesNone([1, 2, 3, 4], "None") ➞ [1, 2, 3, 4]
+
+// ans)=>
+function ascDesNone(arr,order){
+    if(order == "Asc" || order == "None"){
+        return arr.sort((a,b)=>a-b)
+    }else if(order == "Des"){
+       return arr.sort((a,b)=>b-a)
+    }
 }
 
-console.log(testJackpot(["@", "@", "@", "@"]))// ➞ true
-console.log(testJackpot(["abc", "abc", "abc", "abc"])) // ➞ true
-console.log(testJackpot(["SS", "SS", "SS", "SS"])) // ➞ true
-console.log(testJackpot(["&&", "&", "&&&", "&&&&"])) // ➞ false
-console.log(testJackpot(["SS", "SS", "SS", "Ss"])) // ➞ false
+console.log(ascDesNone([4, 3, 2, 1], "Asc" )) // ➞ [1, 2, 3, 4]
+console.log(ascDesNone([7, 8, 11, 66], "Des")) // ➞ [66, 11, 8, 7]
+console.log(ascDesNone([1, 2, 3, 4], "None")) // ➞ [1, 2, 3, 4]
