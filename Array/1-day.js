087406b68fa0692bdf22be3552512bee9fd62a2d
meekailkhan <meekailkhan123aar@gmail.com>
2024-05-13 22:40:42 +0530

    in this repo we solve daily random questions of javascript

--- Array/1-day.js
+++ Array/1-day.js
@@ -101,14 +101,14 @@ function nSidedShape(n){
 // Notes
 // No test case will contain punctuation.
 // Arrays can be of various lengths.
-function sporter(str){
+function eachLength(str){
     return str.length
 }
 
 function wordLengths(arr){
     let lengthArr = [];
     for(let i=0;i<arr.length;i++){
-        lengthArr.push(sporter(arr[i]))
+        lengthArr.push(eachLength(arr[i]))
     }
     return lengthArr;
 }
@@ -125,3 +125,88 @@ function wordLengths(arr){
 // console.log(wordLengths(["hello", "world"])) //➞ [5, 5]
 // console.log(wordLengths(["Halloween", "Thanksgiving", "Christmas"])) //➞ [9, 12, 9]
 // console.log(wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"])) //➞ [3, 5, 9, 4, 2, 3, 8]
+// ------------------------------------------------------------------------------------------------------------------------
+// Question)=> Create a function that takes as a parameter an array of "stringified" numbers and returns an array of numbers.
+
+// Example:
+
+// ["1", "3", "3.6"] ➞ [1, 3, 3.6]
+// Examples
+// toNumberArray(["9.4", "4.2"]) ➞ [9.4, 4.2]
+
+// toNumberArray(["91", "44"]) ➞ [91, 44]
+
+// toNumberArray(["9.5", "8.8"]) ➞ [9.5, 8.8]
+// Notes
+// Some inputs are floats.
+function toNumberArray(arr){
+    return arr.map(item => item = +item)
+}
+
+function toNumberArray(arr){
+    for(let i=0;i<arr.length;i++){
+        arr[i] = +arr[i];
+    }
+    return arr;
+}
+
+// console.log(toNumberArray(["9.4", "4.2"])) //➞ [9.4, 4.2]
+// console.log(toNumberArray(["91", "44"])) //➞ [91, 44]
+// console.log(toNumberArray(["9.5", "8.8"])) //➞ [9.5, 8.8]
+// ---------------------------------------------------------------------------------------------------------------
+// Question)=> Given an array of numbers, create a function which returns the same array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+
+// Examples
+// addIndexes([0, 0, 0, 0, 0]) ➞ [0, 1, 2, 3, 4]
+
+// addIndexes([1, 2, 3, 4, 5]) ➞ [1, 3, 5, 7, 9]
+
+// addIndexes([5, 4, 3, 2, 1]) ➞ [5, 5, 5, 5, 5]
+// Notes
+// You'll only get numbers in the array.
+
+function addIndexes(arr){
+    for(let i=0;i<arr.length;i++){
+        arr[i] = arr[i]+i
+    }
+    return arr;
+}
+
+function addIndexes(arr){
+    return arr.map((item,index) => item = item+index)
+}
+
+// console.log(addIndexes([0, 0, 0, 0, 0])) //➞ [0, 1, 2, 3, 4]
+// console.log(addIndexes([1, 2, 3, 4, 5])) //➞ [1, 3, 5, 7, 9]
+// console.log(addIndexes([5, 4, 3, 2, 1])) //➞ [5, 5, 5, 5, 5]
+// --------------------------------------------------------------------------------------------------------------------------------
+// Question)=> Write a function that returns the sum of elements greater than 5, in the given array .
+
+// Examples
+// sumFive([1, 5, 20, 30, 4, 9, 18]) ➞ 77
+
+// sumFive([1, 2, 3, 4]) ➞ 0
+
+// sumFive([10, 12, 28, 47, 55, 100]) ➞ 252
+// Notes
+// Find all the elements greater than 5, not the elements greater than or equal to 5.
+
+function sumFive(arr){
+    let res = 0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>5){
+            res += arr[i]
+        }
+    }
+    return res;
+}
+
+function sumFive(arr){
+    let res = 0;
+    arr.map(item => item>5 ? res+= item : res)
+    return res;
+}
+
+// console.log(sumFive([1, 5, 20, 30, 4, 9, 18])) //➞ 77
+// console.log(sumFive([1, 2, 3, 4])) //➞ 0
+// console.log(sumFive([10, 12, 28, 47, 55, 100])) //➞ 252
