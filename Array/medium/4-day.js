2acc1408ff50249447f987b658a3e55ed2d3ba8a
pc <meekailkhan123aar@gmail.com>
2024-10-30 18:01:26 +0530

    edabit

--- Array/medium/4-day.js
+++ Array/medium/4-day.js
@@ -4,11 +4,11 @@
 // For instance, totalVolume([2, 3, 2], [6, 6, 7], [1, 2, 1]) should return 266 since (2 x 3 x 2) + (6 x 6 x 7) + (1 x 2 x 1) = 12 + 252 + 2 = 266.
 
 // Examples
-console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1])) //➞ 63
+// console.log(totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1])) //➞ 63
 
-console.log(totalVolume([2, 2, 2], [2, 1, 1])) //➞ 10
+// console.log(totalVolume([2, 2, 2], [2, 1, 1])) //➞ 10
 
-console.log(totalVolume([1, 1, 1])) //➞ 1
+// console.log(totalVolume([1, 1, 1])) //➞ 1
 // Notes
 // You will be given at least one box.
 // Each box will always have three dimensions included.
@@ -102,6 +102,86 @@ function totalVolume(...boxes){
         }
         res += acc
         i++
+    }
+    return res
+}
+// ---------------------------------------------------------------------
+// 2.Question)=> Index Multiplier
+// Return the sum of all items in an array, where each item is multiplied by its index (zero-based). For empty arrays, return 0.
+
+// Examples
+console.log(indexMultiplier([1, 2, 3, 4, 5])) //➞ 40
+// // (1*0 + 2*1 + 3*2 + 4*3 + 5*4)
+
+console.log(indexMultiplier([-3, 0, 8, -6])) //➞ -2
+// // (-3*0 + 0*1 + 8*2 + -6*3)
+// Notes
+// All items in the array will be integers.
+function indexMultiplier(arr){
+    let res = 0;
+    arr.map((item,index) => {
+        res += item * index
+    })
+    return res
+}
+
+function indexMultiplier(arr){
+    let res = 0;
+    for(let i = 0; i < arr.length ; i++){
+        res += arr[i] * i
+    }
+    return res
+}
+
+function indexMultiplier(arr){
+    let res = 0 ;
+    for(let i in arr){
+        res += arr[i] * i
+    }
+    return res
+}
+
+function indexMultiplier(arr){
+    let res = 0;
+    let i = 0;
+    while(i < arr.length){
+        res += arr[i] * i;
+        i++
+    }
+    return res
+}
+
+function indexMultiplier(arr){
+    let res = arr.reduce((acc,val,i)=>{
+        acc += (val * i)
+        return acc
+    },1)
+    return res
+
+}
+function indexMultiplier(arr){
+    let res = 0;
+    arr.forEach((item,index)=>{
+        res += item * index
+    })
+    return res
+}
+function indexMultiplier(arr){
+    return arr.entries()
+}
+
+function indexMultiplier(arr){
+    let res = arr.map((item,i)=> item * i).reduce((acc,val)=>{
+        acc += val
+        return acc
+    },1)
+    return res
+}
+
+function indexMultiplier(arr){
+    let res = 0;
+    for(let [val,i] of arr.entries()){
+        res += val * i
     }
     return res
 }
