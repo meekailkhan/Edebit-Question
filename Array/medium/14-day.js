e66d8560e67e9361c2458fa9d1ed7506e9b28f76
pc <meekailkhan123aar@gmail.com>
2024-11-23 02:00:47 +0530

    edabit

--- Array/medium/14-day.js
+++ Array/medium/14-day.js
@@ -175,30 +175,30 @@ function identicalFilter(arr) {
 // Suppose an image can be represented as a 2D array of 0s and 1s. Write a function to reverse an image. Replace the 0s with 1s and vice versa.
 
 // Examples
-console.log(reverseImage([
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]
-])) //➞ [
-//   [0, 1, 1],
-//   [1, 0, 1],
-//   [1, 1, 0]
-// ]
+// console.log(reverseImage([
+//     [1, 0, 0],
+//     [0, 1, 0],
+//     [0, 0, 1]
+// ])) //➞ [
+// //   [0, 1, 1],
+// //   [1, 0, 1],
+// //   [1, 1, 0]
+// // ]
 
-console.log(reverseImage([
-    [1, 1, 1],
-    [0, 0, 0]
-])) //➞ [
-//   [0, 0, 0],
-//   [1, 1, 1]
-// ]
+// console.log(reverseImage([
+//     [1, 1, 1],
+//     [0, 0, 0]
+// ])) //➞ [
+// //   [0, 0, 0],
+// //   [1, 1, 1]
+// // ]
 
-console.log(reverseImage([
-    [1, 0, 0],
-    [1, 0, 0]
-])) //➞ [
-//   [0, 1, 1],
-//   [0, 1, 1]
+// console.log(reverseImage([
+//     [1, 0, 0],
+//     [1, 0, 0]
+// ])) //➞ [
+// //   [0, 1, 1],
+// //   [0, 1, 1]
 // ]
 // Notes
 // N/A
@@ -234,4 +234,79 @@ function reverseImage(arr){
         }
     }
     return arr
+}
+// -----------------------------------------------------------------------------------------------------------------
+// 6Question)=> Record Temperatures
+// You are given two arrays that each contain data that represents the min and max weather temperatures for each day of the week.
+
+// The records array contains the all-time record low/high temperatures for that day of the week.
+
+// [[record low, record high], ...]
+// The current week array contains the daily low/high temperatures for each day of the current week.
+
+// [[daily low, daily high], ...]
+// A daily high temperature is considered a new record high if it is higher than the record high for that day of the week. A daily low temperature is considered a new record low if it is lower than the record low for that day of the week.
+
+// Compare the daily low/high temperatures of the current week to the record lows/highs and return an array with the updated record temperatures.
+
+// There may be multiple record temperatures in a week.
+// If there are no broken records return the original records array.
+// Example
+// //             sun       mon      tues       wed      thur      fri       sat
+console.log(recordTemps([[34, 82], [24, 82], [20, 89],  [5, 88],  [9, 88], [26, 89], [27, 83]],
+            [[44, 72], [19, 70], [40, 69], [39, 68], [33, 64], [36, 70], [38, 69]]))
+
+// ➞           [[34, 82], [19, 82], [20, 89], [5, 88], [9, 88], [26, 89], [27, 83]]
+// The previous record low for Monday was 24. The current week's low for Monday was 19. So 19 replaces 24 as Monday's new record low.
+
+// Notes
+// There won't be a record high and record low set on the same day.
+// Index 0 will always be the low and index 1 will always be the high [low, high].
+// For reference these temps are °F but you do not need to convert any temperatures.
+function recordTemps(records,currentWeek){
+    for(let i = 0 ; i < records.length ; i++){
+        if(currentWeek[i][0]  < records[i][0]){
+            records[i][0] = currentWeek[i][0]
+        }
+        if(currentWeek[i][1] > records[i][1]){
+            records[i][1] = currentWeek[i][1]
+        }
+    }
+    return records
+}
+
+function recordTemps(records,currentWeek){
+    let res = records.map((item,index)=>{
+        let newlow = Math.min(item[0],currentWeek[index][0]);
+        let newhigh = Math.max(item[1],currentWeek[index][0]);
+        return [newlow,newhigh]
+    })
+
+    return res
+}
+
+function recordTemps(records,currentWeek){
+    let res = records.reduce((acc,val,i)=>{
+        let newLow = Math.min(val[0],currentWeek[i][0]);
+        let newhigh = Math.max(val[1],currentWeek[i][1]);
+        acc.push([newLow,newhigh]);
+        return acc
+    },[])
+    return res
+}
+function recordTemps(records,currentWeek){
+    if(records.length === 0) return [];
+    let res = [];
+    let newLow = Math.min(records[0][0],currentWeek[0][0]);
+    let newhigh = Math.max(records[0][1],currentWeek[0][1]);
+    res.push([newLow,newhigh]);
+    return res.concat(recordTemps(records.slice(1),currentWeek.slice(1)));
+}
+
+function recordTemps(records,currentWeek){
+    let res = records.map(([low,high],i)=>[
+        Math.min(low,currentWeek[i][0]),
+        Math.max(high,currentWeek[i][1])
+    ])
+    return res
 }
