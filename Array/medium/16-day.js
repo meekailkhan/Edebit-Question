9521418f89daf9fea5c4939bf11419084532f829
pc <meekailkhan123aar@gmail.com>
2024-12-31 01:08:11 +0530

    edabit

--- Array/medium/16-day.js
+++ Array/medium/16-day.js
@@ -76,11 +76,11 @@ function nextInLine(arr,num){
 // A mountain can be considered scalable if each number is within 5 units of the next number in either direction.
 
 // Examples
-console.log(isScalable([1, 2, 4, 6, 7, 8])) //➞ true
+// console.log(isScalable([1, 2, 4, 6, 7, 8])) //➞ true
 
-console.log(isScalable([40, 45, 50, 45, 47, 52])) //➞ true
+// console.log(isScalable([40, 45, 50, 45, 47, 52])) //➞ true
 
-console.log(isScalable([2, 9, 11, 10, 18, 21])) //➞ false
+// console.log(isScalable([2, 9, 11, 10, 18, 21])) //➞ false
 // Notes
 // The array may start at any number and can be any length.
 function isScalable(arr){
