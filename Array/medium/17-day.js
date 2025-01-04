e0b0a5a3ded6d4b9537f287749b1a925acff5445
pc <meekailkhan123aar@gmail.com>
2025-01-04 12:12:26 +0530

    edabit

--- Array/medium/17-day.js
+++ Array/medium/17-day.js
@@ -97,42 +97,42 @@ function moveToEnd(arr,target){
 // Given an array of arrays, return a new array of arrays containing every element, except for the outer elements.
 
 // Examples
-console.log(peelLayerOff([
-  ["a", "b", "c", "d"],
-  ["e", "f", "g", "h"],
-  ["i", "j", "k", "l"],
-  ["m", "n", "o", "p"]
-])) //➞ [
-//   ["f", "g"],
-//   ["j", "k"]
-// ]
+// console.log(peelLayerOff([
+//   ["a", "b", "c", "d"],
+//   ["e", "f", "g", "h"],
+//   ["i", "j", "k", "l"],
+//   ["m", "n", "o", "p"]
+// ])) //➞ [
+// //   ["f", "g"],
+// //   ["j", "k"]
+// // ]
 
-console.log(peelLayerOff([
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25],
-  [26, 27, 28, 29, 30],
-  [31, 32, 33, 34, 35]
-])) //➞ [
-//   [7, 8, 9],
-//   [12, 13, 14],
-//   [17, 18, 19],
-//   [22, 23, 24],
-//   [27, 28, 29]
-// ]
+// console.log(peelLayerOff([
+//   [1, 2, 3, 4, 5],
+//   [6, 7, 8, 9, 10],
+//   [11, 12, 13, 14, 15],
+//   [16, 17, 18, 19, 20],
+//   [21, 22, 23, 24, 25],
+//   [26, 27, 28, 29, 30],
+//   [31, 32, 33, 34, 35]
+// ])) //➞ [
+// //   [7, 8, 9],
+// //   [12, 13, 14],
+// //   [17, 18, 19],
+// //   [22, 23, 24],
+// //   [27, 28, 29]
+// // ]
 
-console.log(peelLayerOff([
-  [true, false, true],
-  [false, false, true],
-  [true, true, true]
-])) //➞ [[false]]
+// console.log(peelLayerOff([
+//   [true, false, true],
+//   [false, false, true],
+//   [true, true, true]
+// ])) //➞ [[false]]
 
-console.log(peelLayerOff([
-  ["hello", "world"],
-  ["hello", "world"]
-])) //➞ []
+// console.log(peelLayerOff([
+//   ["hello", "world"],
+//   ["hello", "world"]
+// ])) //➞ []
 // Notes
 // The 2D grid is always a rectangular/square shape.
 // Always return some form of nested array, unless there are no elements. In that case, return an empty array.Peeling off the Outer Layers
@@ -140,3 +140,61 @@ function peelLayerOff(arr){
     if(arr.length <= 2 || arr[0].length <= 2) return [];
     return arr.slice(1,-1).map(item => item.slice(1,-1))
 }
+// --------------------------------------------------------------------------------------------------------
+// Lonely Integer
+// You are given an array of integers having both negative and positive values, except for one integer which can be negative or positive. Create a function to find out that integer.
+
+// Examples
+// console.log(lonelyInteger([1, -1, 2, -2, 3])) //➞ 3
+// // // 3 has no matching negative appearance.
+
+// console.log(lonelyInteger([-3, 1, 2, 3, -1, -4, -2])) //➞ -4
+// // // -4 has no matching positive appearance.
+
+// console.log(lonelyInteger([-9, -105, -9, -9, -9, -9, 105])) //➞ -9
+// // Notes
+// N/A
+function lonelyInteger(arr) {
+  const seen = new Set();
+  
+  for (let num of arr) {
+    if (seen.has(-num)) {
+      seen.delete(-num); // remove the matching counterpart
+    } else {
+      seen.add(num); // add the number if no matching counterpart
+    }
+  }
+  
+  // The only number left in the set is the lonely integer
+  return [...seen][0];
+}
+// ---------------------------------------------------------------------------------------
+// Sum of Two Numbers in Array Equal to Given Number
+// Create a function that takes an array of numbers arr and a number n. Return true if the sum of any two elements is equal to the given number. Otherwise, return false.
+
+// Examples
+// Notes
+// N/A
+function checkSum(arr,num){
+    
+    for(let i = 0 ; i < arr.length ; i++){
+        let ele = arr[i];
+        for(let j = 0 ; j < arr.length ; j++){
+            if(i !== j){
+                if((ele + arr[j]) === num){
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
+console.log(checkSum([10, 12, 4, 7, 9, 11], 16)) //➞ true
+
+console.log(checkSum([4, 5, 6, 7, 8, 9], 13)) //➞ true
+
+console.log(checkSum([0, 98, 76, 23], 174)) //➞ true
+
+console.log(checkSum([0, 9, 7, 23, 19, 18, 17, 66], 39)) //➞ false
+
+console.log(checkSum([2, 8, 9, 12, 45, 78], 1)) //➞ false
