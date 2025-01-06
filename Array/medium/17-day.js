c90109ad6c96ee4f1cc487bbc5421da18b502651
pc <meekailkhan123aar@gmail.com>
2025-01-06 17:19:56 +0530

    edabit

--- Array/medium/17-day.js
+++ Array/medium/17-day.js
@@ -175,26 +175,26 @@ function lonelyInteger(arr) {
 // Examples
 // Notes
 // N/A
-// function checkSum(arr,num){
+function checkSum(arr,num){
     
-//     for(let i = 0 ; i < arr.length ; i++){
-//         let ele = arr[i];
-//         for(let j = 0 ; j < arr.length ; j++){
-//             if(i !== j){
-//                 if((ele + arr[j]) === num){
-//                     return true
-//                 }
-//             }
-//         }
-//     }
-//     return false
-// }
-console.log(checkSum([10, 12, 4, 7, 9, 11], 16)) //➞ true
+    for(let i = 0 ; i < arr.length ; i++){
+        let ele = arr[i];
+        for(let j = 0 ; j < arr.length ; j++){
+            if(i !== j){
+                if((ele + arr[j]) === num){
+                    return true
+                }
+            }
+        }
+    }
+    return false
+}
+// console.log(checkSum([10, 12, 4, 7, 9, 11], 16)) //➞ true
 
-console.log(checkSum([4, 5, 6, 7, 8, 9], 13)) //➞ true
+// console.log(checkSum([4, 5, 6, 7, 8, 9], 13)) //➞ true
 
-console.log(checkSum([0, 98, 76, 23], 174)) //➞ true
+// console.log(checkSum([0, 98, 76, 23], 174)) //➞ true
 
-console.log(checkSum([0, 9, 7, 23, 19, 18, 17, 66], 39)) //➞ false
+// console.log(checkSum([0, 9, 7, 23, 19, 18, 17, 66], 39)) //➞ false
 
-console.log(checkSum([2, 8, 9, 12, 45, 78], 1)) //➞ false
+// console.log(checkSum([2, 8, 9, 12, 45, 78], 1)) //➞ false
