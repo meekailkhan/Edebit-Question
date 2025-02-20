125a68b00b4c9bbdd41f044ac9677aad4981d8c4
pc <meekailkhan123aar@gmail.com>
2025-02-21 00:30:32 +0530

    edabit

--- Array/medium/19-day.js
+++ Array/medium/19-day.js
@@ -33,13 +33,77 @@ function checkSum(arr,target){
 // The object key will be the number converted to a string.
 // The value will be the corresponding character code converted to a string (check ASCII table).
 // Return an array of the resulting objects.
-// Examples
-console.log(numObj([118, 117, 120])) //➞ [{'118':'v'}, {'117':'u'}, {'120':'x'}]
+// // Examples
+// console.log(numObj([118, 117, 120])) //➞ [{'118':'v'}, {'117':'u'}, {'120':'x'}]
 
-console.log(numObj([101, 121, 110, 113, 103])) //➞ [{'101':'e'}, {'121':'y'}, {'110':'n'}, {'113':'q'}, {'103':'g'}]
+// console.log(numObj([101, 121, 110, 113, 103])) //➞ [{'101':'e'}, {'121':'y'}, {'110':'n'}, {'113':'q'}, {'103':'g'}]
 
-console.log(numObj([118, 103, 110])) //➞ [{"118":"v"}, {"103":"g"}, {"110":"n"}]
+// console.log(numObj([118, 103, 110])) //➞ [{"118":"v"}, {"103":"g"}, {"110":"n"}]
 // Notes
 // All inputs will be an array of numbers.
 // All character codes are valid lower case letters.
 // The input array will not be empty.
+function numObj(arr){
+    let obj = {}
+    for(let i = 0 ; i < arr.length ; i++){
+        obj[arr[i]] = String.fromCharCode(arr[i])
+    }
+    return obj
+}
+// ----------------------------------------------------------------------------------------------------------
+// Flash Cards
+// Create a function that outputs the results of a flashcard. A flashcard is an array of three elements: a number, an operator symbol, and another number. Return the mathematical result of that expression.
+
+// There are 4 operators: + (addition), - (subtraction), x (multiplication), and / (division). If the flashcard displays a number being divided by zero, e.g. [3, "/", 0], then return undefined. For division, round to the hundredths place. So [10, "/", 3] should return 3.33.
+
+// Examples
+// console.log(flash([3, "*", 7])) //➞ 21
+
+// console.log(flash([5, "+", 7])) //➞ 12
+
+// console.log(flash([10, "-", 9])) //➞ 1
+
+// console.log(flash([10, "/", 0])) //➞ undefined
+
+// console.log(flash([10, "/", 3])) //➞ 3.33
+// Notes
+// Flash cards contain only zero or positive numbers.
+function flash(arr){
+    let res = arr.join('');
+    return eval(res)
+}
+// --------------------------------------------------------------------------------------
+// Potential Friend?
+// Given two arrays of two people's different interests, return whether both people have at least two things in common or have exact interests. Return true if there's a potential friend!
+
+// Examples
+console.log(isPotentialFriend(
+  ["sports", "music", "chess"],
+  ["coding", "music", "netflix", "chess"]
+)) //➞ true
+
+console.log(isPotentialFriend(
+  ["cycling", "technology", "drawing"],
+  ["dancing", "drawing"]
+)) //➞ false
+
+console.log(isPotentialFriend(
+  ["history"],
+  ["history"]
+)) //➞ true
+// Notes
+// Arrays of interests may have varied lengths.
+function isPotentialFriend(arr1,arr2){
+    let res = 0;
+    if(arr1.length == 1 && arr2.length == 1){
+        return arr1.includes(arr2[0])
+    }
+    let lengthyArr = arr1.length > arr2.length ? arr1 : arr2;
+    let smallArr = arr1.length < arr2.length ? arr1 : arr2
+    for(let i = 0 ; i < lengthyArr.length ; i++){
+        if(smallArr.includes(lengthyArr[i])){
+            res++
+        }
+    }
+    return res > 1
+}
