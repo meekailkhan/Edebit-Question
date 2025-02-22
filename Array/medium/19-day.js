69e9b766a5829c4b5a81321f6fd2d41100d1081b
pc <meekailkhan123aar@gmail.com>
2025-02-23 01:47:15 +0530

    edabit

--- Array/medium/19-day.js
+++ Array/medium/19-day.js
@@ -107,6 +107,27 @@ function isPotentialFriend(arr1,arr2){
     }
     return res > 1
 }
+// ------------------------------------------------------------------------------------------------------------------
+// String Match by Two Letters
+// Create a function that takes two strings, a and b. Return the number of times the two strings contain the same two letters at the same index. The two letters must appear at consecutive indices.
 
+// For example, if a = "bboiizz" and b = "bbuiiz", your function should return 3, since the "bb", "ii", and "iz" appear at the same place in both strings.
 
+// Examples
+console.log(strMatchBy2char("yytaazz", "yyjaaz")) //➞ 3
 
+console.log(strMatchBy2char("edabit", "ed")) //➞ 1
+
+console.log(strMatchBy2char("", "")) //➞ 0
+// Notes
+// Don't forget to return the result.
+function strMatchBy2char(str1,str2){
+    let count = 0;
+    let minLength = Math.min(str1.length, str2.length);
+    for(let i = 0 ; i < minLength ; i++){
+        if(str1[i] + str1[i+1] === str2[i] + str2[i+1]){
+            count++
+        }
+    }
+    return count
+}
