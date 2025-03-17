b87c3967ba02a78ac05ec7263557206574fb236f
pc <meekailkhan123aar@gmail.com>
2025-03-17 23:16:40 +0530

    edabit

--- Array/medium/20-day.js
+++ Array/medium/20-day.js
@@ -115,11 +115,11 @@ function findSingleNumber(arr){
 // Write a function that finds the longest word in a sentence and returns it. If two or more words are the biggest, return the word closest to the start of the sentence. Characters such as apostrophe, commas, periods, etc count as letters (e.g. O'Connor is 8 characters long).
 
 // Examples
-console.log(longestWord("Hello darkness my old friend")) //➞ "darkness"
+// console.log(longestWord("Hello darkness my old friend")) //➞ "darkness"
 
-console.log(longestWord("Hello there mate")) //➞ "Hello"
+// console.log(longestWord("Hello there mate")) //➞ "Hello"
 
-console.log(longestWord("Kayla's toy is plastic")) //➞ "Kayla's"
+// console.log(longestWord("Kayla's toy is plastic")) //➞ "Kayla's"
 // Notes
 // N/A
 function longestWord(str){
