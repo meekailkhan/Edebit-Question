6d67c593c1ebfae47a1b86ad5b5da6e230271337
pc <meekailkhan123aar@gmail.com>
2025-03-20 00:11:40 +0530

    edabit

--- Array/medium/21-day.js
+++ Array/medium/21-day.js
@@ -2,20 +2,20 @@
 // You call your spouse to inform his/her most precious item is gone! Given an object of stolen items, return the most expensive item on the list.
 
 // Examples
-console.log(mostExpensiveItem({
-  piano: 2000,
-})) // ➞ "piano"
+// console.log(mostExpensiveItem({
+//   piano: 2000,
+// })) // ➞ "piano"
 
-console.log(mostExpensiveItem({
-  tv: 30,
-  skate: 20,
-})) // ➞ "tv"
+// console.log(mostExpensiveItem({
+//   tv: 30,
+//   skate: 20,
+// })) // ➞ "tv"
 
-console.log(mostExpensiveItem({
-  tv: 30,
-  skate: 20,
-  stereo: 50,
-})) // ➞ "stereo"
+// console.log(mostExpensiveItem({
+//   tv: 30,
+//   skate: 20,
+//   stereo: 50,
+// })) // ➞ "stereo"
 // Notes
 // There will only be one most valuable item (no ties).
 // The object will always contain at least one item (no empty objects).
@@ -33,5 +33,33 @@ function mostExpensiveItem(obj){
         }
     }
     return item
-
 }
+// ------------------------------------------------------------------------------------
+// Burglary Series (06): Convert to Number
+// You prepare a list to send to the insurance company. As you finish, you notice you misformatted it. Given an object with at least one key/value pair, convert all the values to numbers.
+
+// Examples
+console.log(convertToNumber({ piano: "200" })) //➞ { piano: 200 }
+
+console.log(convertToNumber({ piano: "200", tv: "300" })) //➞ { piano: 200, tv: 300 }
+
+console.log(convertToNumber({ piano: "200", tv: "300", stereo: "400" })) //➞ { piano: 200, tv: 300, stereo: 400 }
+// Notes
+// N/A
+
+function convertToNumber(obj){
+    let keys = Object.keys(obj);
+
+    for(let i = 0 ; i < keys.length ; i++){
+        obj[keys[i]] = +obj[keys[i]]
+    }
+    return obj
+}
+
+function convertToNumber(obj){
+    for(key in obj){
+        obj[key] = +obj[key]
+    }
+    return obj
+}
+
